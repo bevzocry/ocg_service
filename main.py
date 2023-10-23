1073bcfc07a9a7327057b7ee0ae738858959bcc8
bevzocry <vascodeagilare@gmail.com>
2023-10-23 12:21:09 +0500

    прикрепеление файлов

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 
 def main(page: ft.Page):
     page.title = 'Заявка в технический отдел OCG'
-    #page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    file_picker = ft.FilePicker()
+    page.overlay.append(file_picker)
 
     def on_company_change(e):
         drop_depart.options.clear()
@@ -24,10 +25,17 @@ def main(page: ft.Page):
                 drop_subject.options.append(ft.dropdown.Option(key=d['id'], text=d['name']))
         page.update()
 
-    #Заголовок
+    def on_phone_change(e):
+        if not phone_field.value.isdigit():
+            phone_field.error_text = "Неверный формат номера телефона"
+        else:
+            phone_field.error_text = ""
+        page.update()
+
+    # Заголовок
     cont_titl = ft.Row(controls=[ft.Text('Заявка в технический отдел', style=ft.TextThemeStyle.TITLE_LARGE)], alignment=ft.MainAxisAlignment.CENTER, height=50)
 
-    #Компания
+    # Компания
     cont_company = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
     drop_company = ft.Dropdown(label='Организация', width=500, on_change=on_company_change) # alignment=ft.alignment.center
     resp = requests.get('http://ws_guest:@192.168.10.123/workbase/hs/it/get_company')
@@ -37,12 +45,12 @@ def main(page: ft.Page):
             drop_company.options.append(op)
     cont_company.controls.append(drop_company)
 
-    #Подразделение
+    # Подразделение
     cont_depart = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
-    drop_depart = ft.Dropdown(label='Подразделение', width=500) #alignment=ft.alignment.center
+    drop_depart = ft.Dropdown(label='Подразделение', width=500)
     cont_depart.controls.append(drop_depart)
 
-    #Тема
+    # Тема
     cont_theme = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
     drop_theme = ft.Dropdown(label='Тема обращения', width=500, on_change=on_theme_change)  # alignment=ft.alignment.center
     resp_theme = requests.get('http://ws_guest:@192.168.10.123/workbase/hs/it/get_subject')
@@ -60,7 +68,7 @@ def main(page: ft.Page):
 
     # Номер телефона
     cont_phone = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
-    phone_field = ft.TextField(label='Номер телефона', width=500, prefix_text='7', icon=ft.icons.PHONE)
+    phone_field = ft.TextField(label='Номер телефона', width=500, prefix_text='7', icon=ft.icons.PHONE, max_length=10, on_change=on_phone_change)
     cont_phone.controls.append(phone_field)
 
     # Комментарий
@@ -68,7 +76,14 @@ def main(page: ft.Page):
     comment_field = ft.TextField(label='Комментарий', width=500, multiline=True, min_lines=1, max_lines=6)
     cont_comment.controls.append(comment_field)
 
-    page.add(cont_titl, cont_company, cont_depart, cont_theme, cont_subject, cont_phone, cont_comment)
+    # FilePicker
+    cont_file = ft.Row(alignment=ft.MainAxisAlignment.CENTER)
+    file_button = ft.ElevatedButton("Choose files...",
+        on_click=lambda _: file_picker.pick_files(allow_multiple=True, allowed_extensions=['jpg']),
+        right=0)
+    cont_file.controls.append(file_button)
+
+    page.add(cont_titl, cont_company, cont_depart, cont_theme, cont_subject, cont_phone, cont_comment, cont_file)
 
 
-ft.app(target=main)
+ft.app(target=main, view=ft.AppView.WEB_BROWSER)
