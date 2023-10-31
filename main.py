3cc6ab784fb630e0769f2c96323722360a546eea
bevzocry <vascodeagilare@gmail.com>
2023-10-31 11:59:07 +0500

    1159

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import flet as ft
 import requests
 import base64
-from typing import Dict
+from pathlib import Path
 
+
+uploads_dir = "uploads"
 
 def main(page: ft.Page):
-    prog_bars: Dict[str, ft.ProgressRing] = {}
+    prog_bars = {}
     page.title = 'Заявка в технический отдел OCG'
     page.theme = ft.theme.Theme(color_scheme_seed='blue')
     page.scroll = ft.ScrollMode.ADAPTIVE
@@ -30,9 +32,35 @@ def main(page: ft.Page):
                         img1.src_base64 = base64.b64encode(image.read()).decode('utf-8')
                     else:
                         break
+                im_row.visible = True
+                im_col.visible = False
             else: # web
                 prog_bars.clear()
+                im_col.controls.clear()
+                for f in e.files:
+                    prog = ft.ProgressRing(value=0, bgcolor="#eeeeee", width=20, height=20)
+                    prog_bars[f.name] = prog
+                    im_col.controls.append(ft.Row([prog, ft.Text(f.name)]))
+                im_row.visible = False
+                im_col.visible = True
             page.update()
+
+    def upload_files():
+        uf = []
+        if file_picker.result is not None and file_picker.result.files is not None:
+            for f in file_picker.result.files:
+                uf.append(
+                    ft.FilePickerUploadFile(
+                        f.name,
+                        upload_url=page.get_upload_url(f.name, 600),
+                    )
+                )
+            file_picker.upload(uf)
+        return uf
+        
+    def on_upload_progress(e: ft.FilePickerUploadEvent):
+        prog_bars[e.file_name].value = e.progress
+        prog_bars[e.file_name].update()
 
     def on_company_change(e):
         drop_depart.options.clear()
@@ -57,7 +85,15 @@ def main(page: ft.Page):
             phone_field.error_text = ""
         page.update()
 
-    file_picker = ft.FilePicker(on_result=on_dialog_result)
+    def on_submit(e):
+        path = Path(Path.cwd(), uploads_dir)
+        for f in upload_files():
+            image = open(str(path) + '\\' + f.name, 'rb') #open binary file in read mode
+            img_base64 = base64.b64encode(image.read()).decode('utf-8')
+            page.add(ft.Text(img_base64))
+        page.update()
+
+    file_picker = ft.FilePicker(on_result=on_dialog_result, on_upload=on_upload_progress)
     page.overlay.append(file_picker)
 
     # Заголовок
@@ -96,11 +132,19 @@ def main(page: ft.Page):
     file_button = ft.ElevatedButton("Прикрепить файлы",
         on_click=lambda _: file_picker.pick_files(allow_multiple=True, file_type=ft.FilePickerFileType.IMAGE), icon=ft.icons.UPLOAD_FILE)
     
+    # for attached images
     img = ft.Image(src_base64='', width=150)
     img1 = ft.Image(src_base64='', width=150)
-    im_row = ft.Row(controls=[img, img1])
+    im_row = ft.Row(controls=[img, img1], visible=False)
 
-    page.add(cont_titl, drop_company, drop_depart, drop_theme, drop_subject, phone_field, comment_field, file_button, im_row)
+    # for attached images web
+    im_col = ft.Column(controls=[])
+
+    # submit
+    submit_button = ft.ElevatedButton("Создать заявку",
+        on_click=on_submit, icon=ft.icons.CREATE)
+
+    page.add(cont_titl, drop_company, drop_depart, drop_theme, drop_subject, phone_field, comment_field, file_button, im_row, im_col, submit_button)
 
 
-ft.app(target=main, port=8080, view=ft.AppView.WEB_BROWSER)
+ft.app(target=main, upload_dir=uploads_dir, port=8080, view=ft.AppView.WEB_BROWSER)
